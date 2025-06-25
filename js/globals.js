86ba738694e29d49316e575cd54035bfc9362d04
infiniteOedipus <xeno101blade02@gmail.com>
2025-06-25 11:29:34 -0700

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,3 +1,19 @@
 const gameWindow = document.getElementById("gameWindow");
 const game = gameWindow.getContext("2d");
 
+//Button Inputs
+const keys = {};
+
+window.addEventListener("keydown", e => {
+        keys[e.key] = true;
+});
+
+window.addEventListener("keyup", e => {
+        keys[e.key] = false;
+});
+//Initialize
+function initialize() {
+	console.log("gameLoop initializing")
+	changeGameState(gameState.Attack)
+	requestAnimationFrame(gameLoop)
+}
