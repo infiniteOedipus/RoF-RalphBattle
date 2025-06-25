25e6cc5314e2473917e0411e03bd27da46472062
infiniteOedipus <xeno101blade02@gmail.com>
2025-06-25 13:41:10 -0700

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,6 +1,11 @@
 const gameWindow = document.getElementById("gameWindow");
 const game = gameWindow.getContext("2d");
 
+game.imageSmoothingEnabled       = false;
+game.webkitImageSmoothingEnabled = false;
+game.mozImageSmoothingEnabled    = false;
+game.msImageSmoothingEnabled     = false;
+
 //Button Inputs
 const keys = {};
 
